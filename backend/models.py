e3b70d018c4069fad526bc9af25e12718e36ccbb
Arbab <arbabhusain200@gmail.com>
2025-07-30 22:13:37 -0400

    updated models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from app import db
 from flask_bcrypt import generate_password_hash, check_password_hash
 
-class User(db.model):
+class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   password_hash = db.Column(db.String(128), nullable=False)
@@ -12,6 +12,11 @@ class User(db.model):
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
 
+  def to_json(self):
+    return {
+        "id": self.id,
+        "username": self.username
+    }
 
 
 
